b66581b263cfb0f7ac08e4e63baf18e8ab555a9b
Linus <36757513+grooof@users.noreply.github.com>
2019-07-31 14:47:11 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from io import open
+
+setup(
+    name='financial_readability',
+    packages=['financial_readability'],
+    version='0.1.0',
+    description='Calculate readability measure from text-paragraph',
+    author='Linus Graf',
+    author_email='linusgraf92@gmail.com',
+    url='https://github.com/grooof/financial_readability',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    package_data={'word_lists': ['word_lists'],'models': ['models']},
+    include_package_data=True,
+    install_requires=['textstat','pyphen', 'nltk', 'sklearn', 
+    'pkg_resources', 'ast', 'spacy', 'numpy', 'pandas', 'pickle'],
+    license='MIT'
+)
